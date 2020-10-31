768e5f4c79b3e63175ea69eb6247dc59a0803c29
Bramantyoadi12 <56532145+Bramantyoadi12@users.noreply.github.com>
2020-10-31 17:04:20 +0700

    First Commit

---
+++ Server_Bramantyo Adi Yoga_20180140052/Form1.cs
@@ -0,0 +1,73 @@
+﻿using ServiceMtk_P1_20180140052;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_Bramantyo_Adi_Yoga_20180140052
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                label2.Text = "Server ON";
+                label3.Text = "Klik OFF untuk menonaktifkan server";
+                button1.Enabled = false;
+                button2.Enabled = true;
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Close();
+                label2.Text = "Server OFF";
+                label3.Text = "Klik ON untuk menghidupkan server";
+                button2.Enabled = false;
+                button1.Enabled = true;
+                Console.WriteLine("Server is OFF!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
